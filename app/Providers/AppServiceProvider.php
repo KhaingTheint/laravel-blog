9496faff5332b86156048267ba4f1269ddb0e4ec
KHAING-PC\user <khaingkyu180810@gmail.com>
2026-06-01 15:41:08 +0900

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use App\Models\Comment;
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Paginator::useBootstrap();
+        Gate::define('comment-delete', function (User $user, Comment $comment) {
+        return $user->id === $comment->user_id;
+        });
+        Gate::define('article-delete', function (User $user, Article $article) {
+        return $user->id === $article->user_id;
+        });
+    
+    }
+}
